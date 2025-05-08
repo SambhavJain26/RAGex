87daa2ec0e9a6a3cd02a6c5feeb9053877706f6b
SambhavJain26 <worksambhav26@gmail.com>
2025-05-08 23:31:36 +0530

    created pipeline with langchain

--- pipeline.py
+++ pipeline.py
@@ -10,8 +10,14 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 from sklearn.manifold import TSNE
 import plotly.graph_objects as go
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
+MODEL = "gpt-4o-mini"
 db_name = "vector_db"
+
+load_dotenv(override=True)
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY', 'your-key-if-not-using-env')
 
 folders = glob.glob("knowledge-base/*")
 text_loader_kwargs = {'encoding': 'utf-8'}
@@ -25,7 +31,7 @@ for folder in folders:
         doc.metadata["doc_type"] = doc_type
         documents.append(doc)
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
 chunks = text_splitter.split_documents(documents)
 
 doc_types = set(chunk.metadata['doc_type'] for chunk in chunks)
@@ -43,3 +49,23 @@ collection = vectorstore._collection
 sample_embedding = collection.get(limit=1, include=["embeddings"])["embeddings"][0]
 dimensions = len(sample_embedding)
 print(f"The vectors have {dimensions:,} dimensions")
+
+# create a new Chat with OpenAI
+llm = ChatOpenAI(temperature=0.7, model_name=MODEL)
+
+# set up the conversation memory for the chat
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+# the retriever is an abstraction over the VectorStore that will be used during RAG
+retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+
+# putting it together: set up the conversation chain with the GPT 4o-mini LLM, the vector store and memory
+conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory)
+
+
+def chat(message, history):
+    result = conversation_chain.invoke({"question": message})
+    return result["answer"]
+
+
+view = gr.ChatInterface(chat, type="messages").launch(inbrowser=True)
